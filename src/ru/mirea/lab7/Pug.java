4a15d716cbf742926c0db903de0d4e5dadc249c0
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-11-27 14:39:21 +0300

    Update Pug.java

--- src/ru/mirea/lab7/Pug.java
+++ src/ru/mirea/lab7/Pug.java
@@ -6,4 +6,8 @@ public class Pug extends Dog{
     Pug(String name, String color){
         super(name, color);
     }
+     public void eat(){
+          System.out.println("Собака ест еду для породы Мопс");
+            happy=happy+10;
+}
 }
