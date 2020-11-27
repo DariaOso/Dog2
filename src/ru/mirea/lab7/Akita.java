e71cfdd5a772063c9a3d02fa03c767b183f54622
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-11-27 14:37:02 +0300

    Update Akita.java

--- src/ru/mirea/lab7/Akita.java
+++ src/ru/mirea/lab7/Akita.java
@@ -6,4 +6,8 @@ public class Akita extends Dog{
     Akita(String name, String color){
         super(name, color);
     }
+    public void eat(){
+          System.out.println("Собака ест еду для породы Акита");
+            happy=happy+10;
+    }
 }
