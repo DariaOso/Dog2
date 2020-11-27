00657acda02eacf42216f0d17e57967c7f26fc8b
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-11-27 14:38:49 +0300

    Update Poodle.java

--- src/ru/mirea/lab7/Poodle.java
+++ src/ru/mirea/lab7/Poodle.java
@@ -5,4 +5,8 @@ public class Poodle extends Dog{
     Poodle(String name, String color){
         super(name, color);
     }
+     public void eat(){
+          System.out.println("Собака ест еду для породы Пудель");
+            happy=happy+10;
+}
 }
